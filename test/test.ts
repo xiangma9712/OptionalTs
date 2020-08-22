bffaaccddb9538115f503fb8ef953e3768c87865
S.Koike <shomakoike0909@gmail.com>
2020-08-22 18:23:46 +0900

    feat: typed funtion args

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,24 @@
 import { describe, it } from "mocha";
 import Optional from "../lib/optional";
-import { expect } from "chai"
+import { expect, assert } from "chai"
+
+describe('test of constructors', () => {
+  it('of constructs Optional with value', () => {
+    const constructed = Optional.of('test');
+    expect(constructed instanceof Optional).to.equal(true);
+  });
+
+  it('ofNullAble constructs Optional', () => {
+    const constructed = Optional.ofNullable(null);
+    expect(constructed instanceof Optional).to.equal(true);
+  });
+
+  it('empty constructs Optional without value', () => {
+    const nullable = Optional.ofNullable(null);
+    const empty = Optional.empty();
+    expect(nullable).to.eql(empty);
+  });
+});
 
 describe('test of emptyness', () => {
   it('Empty Optional returns false by isPressent method', () => {
@@ -36,5 +54,30 @@ describe('test of getting value', () => {
   it('orElseThrow method returns value or throw error', () => {
     expect(Optional.of('test').orElseThrow('empty')).to.equal('test');
     expect(() => Optional.empty().orElseThrow('empty')).to.throw('empty');
+  });
+
+  it('orElseGet method returns value', () => {
+    const fn = () => {
+      return 'empty';
+    }
+    expect(Optional.of('test').orElseGet(fn)).to.equal('test');
+    expect(Optional.empty<string>().orElseGet(fn)).to.equal('empty');
+  });
+});
+
+describe('test of filter, map and ifPresent', () => {
+  it('map : take element of object', () => {
+    const rawObj = {name: 'test', date: '20200822'};
+    expect(Optional.of(rawObj).map(o => o.name)).to.eql(Optional.of('test'));
+  });
+
+  it('filter : take even num', () => {
+    expect(Optional.of(2).filter(n => n % 2 === 0)).to.eql(Optional.of(2));
+    expect(Optional.of(1).filter(n => n % 2 === 0)).to.eql(Optional.empty<number>());
+  });
+
+  it('ifPresent: returns text', () => {
+    expect(Optional.of('test').ifPresent(s => s)).to.equal('test');
+    expect(Optional.empty().ifPresent(s => s)).to.equal(undefined);
   });
 });
